e70462d4109b06884d0667c65981dedc5b3c2bb8
Lucia Alday <123031285+luciaalday@users.noreply.github.com>
2024-04-25 14:51:11 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/********************************************************************
+ * file: main.c
+ * authors: Lucia Alday and Ivonne Lopez
+ * course: ECE 175 Spring 2024
+ * Game of Trains play and specifications:
+ *** (gameplay and rules and how theyre implemented)
+ *** (card struct)
+ *** (deck struct)
+ ********************************************************************/
+
+#include <stdio.h>
+
+typedef struct {
+  int value;
+  char action[15];
+} card;
+
+int main()
+{
+    /**/
+
+    return 0;
+}
